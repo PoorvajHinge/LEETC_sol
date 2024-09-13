7859ac748a041f40f4797256ad9d7467e3377b66
poorvajhinge <74683290+PoorvajHinge@users.noreply.github.com>
2024-09-14 00:40:38 +0530

    Time: 226 ms (33.71%) | Memory: 17.5 MB (82.08%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,7 +1,7 @@
 import sys
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        sys.set_int_max_str_digits(1000000) 
+        sys.set_int_max_str_digits(100000) 
         num1 = ''.join(map(str,num))
         res = int(num1) + k
         new1 = [int(x) for x in str(res)]
